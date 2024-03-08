ca36d2c24165cf187c792de8daec48b28f05d0fe
Nicole T <nbtrieu@uci.edu>
2024-03-08 15:25:22 -0800

    organized files and set up for retrieving author data

--- app/get_pmids_and_addresses.py
+++ app/get_pmids_and_addresses.py
@@ -2,6 +2,7 @@
 import requests
 import xml.etree.ElementTree as ET
 import json
+import subprocess
 import time
 import pandas as pd
 from requests.adapters import HTTPAdapter
@@ -21,7 +22,7 @@ def get_pmids_from_term(api_key: str, term: str):
         "db": "pubmed",
         "term": term,
         "retmode": "json",
-        "retmax": 2,  # limit to 2 PMIDs per term
+        "retmax": 2,  # limit to 2 PMIDs per term to reduce redundancy while also account for cases of missing "affiliation" or "institute" field for the same author
         "api_key": api_key
     }
 
@@ -76,7 +77,8 @@ rneasy_df["FullName"] = rneasy_df.apply(
     lambda row: str(row["FirstName"]) + " " + str(row["LastName"]),
     axis=1
 )
-print(rneasy_df["FullName"])
+author_names = rneasy_df["FullName"].tolist()
+print(author_names)
 
 
 # %%
@@ -88,3 +90,54 @@ def process_pmids_from_author_names(api_key: str, author_names: list):
         all_pmids.extend(idlist)
 
     return all_pmids
+
+
+# %%
+pmids_list = process_pmids_from_author_names(ncbi_api_key, author_names)
+
+# %%
+pmids_df = pd.DataFrame(pmids_list)
+pmids_df.to_pickle('./outputs/addresses_from_names/rneasy_pmids.pkl')
+
+# %%
+pmids_df = pd.read_pickle('./outputs/addresses_from_names/rneasy_pmids.pkl')
+print("PMIDS_DF FROM PICKLE:\n", pmids_df)
+pmid_column_dtype = pmids_df.iloc[:, 0].dtype
+print(f"The data type of the first column is: {pmid_column_dtype}")
+are_all_values_strings = pmids_df.iloc[:, 0].apply(lambda x: isinstance(x, str)).all()
+print(f"Are all values in the first column strings? {are_all_values_strings}")
+
+
+# %%
+def query_pubmed(pmids_list: list) -> list:
+    """
+    Queries the pubmedAuthorAffiliation.py script for authors' affiliations based on PMIDs or DOIs.
+
+    Parameters:
+    values (list): A list of PMID strings
+
+    Returns:
+    list: A list of dictionaries.
+    """
+
+    script_path = "pubmedAuthorAffiliation/pubmedAuthorAffiliation.py"
+    all_author_data = []
+
+    for pmid in tqdm(pmids_list, desc="Querying PubMed"):
+        command = f'poetry run python {script_path} -i {pmid}'
+        result = subprocess.check_output(command, shell=True, text=True, stderr=subprocess.DEVNULL)
+        # print("Attempting to parse JSON:", result[:500])
+        pubmed_data = json.loads(result)
+
+        for author_data in pubmed_data["authorList"]:
+            all_author_data.append(author_data)
+
+    return all_author_data
+
+
+# %%
+pmids_test = ["37971890", "37630833"]
+author_data_test = query_pubmed(pmids_list=pmids_test)
+print(author_data_test)
+
+# %%
