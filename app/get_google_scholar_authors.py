4de87c670a5ebb58d240a9039892895f34e391a0
Nicole T. <nbtrieu@uci.edu>
2024-04-23 14:53:34 -0700

    Revise serpAPI functions

--- app/get_google_scholar_authors.py
+++ app/get_google_scholar_authors.py
@@ -1,6 +1,7 @@
 # %%
 import serpapi
 import json
+import pickle
 import pandas as pd
 from tqdm import tqdm
 
@@ -9,6 +10,24 @@ with open('../config.json', 'r') as file:
 serp_api_key = config['apiKeys']['serp']
 
 client = serpapi.Client(api_key=serp_api_key)
+
+# %%
+test_results = client.search({
+    'engine': 'google_scholar',
+    'q': 'lyticase',
+    'num': '20',
+    'start': 100
+})
+
+print(test_results)
+
+# %%
+print(len(test_results))
+
+# %%
+author_id = test_results["organic_results"][5]["publication_info"]["authors"][0]["author_id"]
+# publication_info = results["organic_results"][1]["publication_info"]
+print(author_id)
 
 
 # %%
@@ -22,17 +41,22 @@ def get_google_scholar_results(keyword: str):
             'num': '20',
             'start': i
         })
-        all_results.extend(results)
+        all_results.append(results)
+
+    return all_results
 
 
 # %%
-lyticase_results = get_google_scholar_results("lyticase")
+lyticase_results = get_google_scholar_results("lyticase")  # list of objects
 
 # %%
 print(lyticase_results)
 
+# %%
+print(len(lyticase_results))
+
 # %% Get author_id from Google Scholar API
-author_id = lyticase_results["organic_results"][1]["publication_info"]["authors"][0]["author_id"]
+author_id = lyticase_results[0]["organic_results"][1]["publication_info"]["authors"][0]["author_id"]
 # publication_info = results["organic_results"][1]["publication_info"]
 print(author_id)
 
@@ -51,26 +75,29 @@ print(author_affiliations)
 def get_author_data(google_scholar_results):
     all_data = []
 
-    for result in tqdm(google_scholar_results.get("organic_results", []),
-                       desc="Extracting author data"):
-        # Check if 'authors' key exists in 'publication_info' before proceeding
-        if "authors" in result.get("publication_info", {}):
-            for author in result["publication_info"]["authors"]:
-                try:
-                    author_id = author.get("author_id")
-                    # Proceed only if author_id is found
-                    if author_id:
-                        author_results = client.search({
-                            "engine": "google_scholar_author",
-                            "author_id": author_id,
-                        })
-                        # Directly append 'author' data assuming it's always present
-                        author_data = author_results["author"]
-                        all_data.append(author_data)
-                except Exception as e:
-                    print(f"Error retrieving data for author ID {author_id}: {e}")
-                    # Skip to the next author if an error occurs
-                    continue
+    for result in tqdm(google_scholar_results, desc="Extracting author data"):
+        # Check if 'publication_info' is in result and 'authors' key exists in 'publication_info' before proceeding
+        organic_results = result.get("organic_results", {})
+        for organic_result in organic_results:
+            publication_info = organic_result.get("publication_info", {})
+            if "authors" in publication_info:
+                for author in publication_info["authors"]:
+                    try:
+                        author_id = author.get("author_id")
+                        # Proceed only if author_id is found
+                        if author_id:
+                            author_results = client.search({
+                                "engine": "google_scholar_author",
+                                "author_id": author_id,
+                            })
+                            # Directly append 'author' data assuming it's always present
+                            author_data = author_results.get("author", {})
+                            if author_data:
+                                all_data.append(author_data)
+                    except Exception as e:
+                        print(f"Error retrieving data for author ID {author_id}: {e}")
+                        # Skip to the next author if an error occurs
+                        continue
 
     return all_data
 
@@ -79,8 +106,20 @@ def get_author_data(google_scholar_results):
 lyticase_author_data = get_author_data(lyticase_results)
 
 # %%
-lyticase_df = pd.DataFrame(lyticase_author_data).drop_duplicates()
+print(lyticase_author_data[0])
+
+# %%
+file_path = './outputs/lyticase_google/lyticase_author_data.pkl'
+with open(file_path, 'wb') as file:
+    pickle.dump(lyticase_author_data, file)
+
+# %%
+lyticase_df = pd.DataFrame(lyticase_author_data)
 print(lyticase_df)
+
+# %%
+# lyticase_df = lyticase_df.drop_duplicates()
+# print(lyticase_df)
 
 # %%
 file_path = './outputs/lyticase_google/lyticase_authors.csv'
